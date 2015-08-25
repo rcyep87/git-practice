2ce6d122fe0f50b855163b5493cac35a837a6f5b
Ryan Yep <ryan.yep@gmail.com>
2015-08-25 15:35:20 -0500

    Hard Mode

--- bouncer.rb
+++ bouncer.rb
@@ -13,6 +13,31 @@
   # this will require conditional check
   # output response based on age
 
+# def check_age(age)
+#   if age < 18
+#     print "Because you're younger than 18, you can do nothing. Sorry!"
+#   elsif (age > 18 && age < 21)
+#     puts "Because you're older than 18, you can vote!"
+#     puts "Because you're older than 18, you can smoke!"
+#   elsif (age >= 21 && age <= 25)
+#     print "Because you're older than 21, you can drink!"
+#   else
+#     print "Because you're older than 25, you can rent a car!"
+#   end
+# end
+
+# print "How old are you? > "
+# age = gets.chomp.to_i
+# puts check_age(age)
+
+# Hard Mode
+# Modify the program to run in a loop constantly asking user's age until the user asks to quit.
+  # tasks
+    # loop do
+    # if continue == "Y"
+
+    # elsif continue == "N"
+
 def check_age(age)
   if age < 18
     print "Because you're younger than 18, you can do nothing. Sorry!"
@@ -26,7 +51,16 @@ def check_age(age)
   end
 end
 
-print "How old are you? > "
-age = gets.chomp.to_i
-puts check_age(age)
+loop do
+  puts "Would you like to check your age? (Y)es or (N)o > "
+  continue = gets.chomp.upcase
+  if continue == "Y"
+    print "How old are you? > "
+    age = gets.chomp.to_i
+    puts check_age(age)
+  elsif continue == "N"
+    puts "Nice knowing ya!"
+    exit
+  end
+end
 
