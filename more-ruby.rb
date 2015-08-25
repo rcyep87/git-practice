8a291a49462a10ed061032c6ba5e19237fe4e7e0
Ryan Yep <ryan.yep@gmail.com>
2015-08-25 11:57:43 -0500

    Continuous loop until user quits

--- more-ruby.rb
+++ more-ruby.rb
@@ -14,23 +14,26 @@ end
 
 system('clear') #system method runs any string you give it on the command line
 puts "Welcome to Square-ulator"
-puts "Would you like to (D)ouble a number, (S) a number or (Q)uit?"
-print "(D, S or Q) > "
-response = gets.chomp.upcase
-if response == "D"
-  print "What number do you want to double? > "
-  response_number = gets.chomp.to_i
-  puts "Double of #{response_number} is #{double(response_number)}"
-elsif response == "S"
-  print "What number do you want to square? > "
-  response_number = gets.chomp.to_i
-  puts "Square of #{response_number} is #{square(response_number)}"
-elsif response == "Q"
-  puts "Goodbye!"
-  exit
-else
-  puts "What?"
-end
+
+loop do #this will continue to run until user wants to quit
+  puts "Would you like to (D)ouble a number, (S) a number or (Q)uit?"
+  print "(D, S or Q) > "
+  response = gets.chomp.upcase
+  if response == "D"
+    print "What number do you want to double? > "
+    response_number = gets.chomp.to_i
+    puts "Double of #{response_number} is #{double(response_number)}"
+  elsif response == "S"
+    print "What number do you want to square? > "
+    response_number = gets.chomp.to_i
+    puts "Square of #{response_number} is #{square(response_number)}"
+  elsif response == "Q"
+    puts "Goodbye!"
+    exit
+  else
+    puts "What?"
+  end#--of if
+end #--of loop
 
 # print "What number do you want to square? > "
 # response_number = gets.chomp.to_i
